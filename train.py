8041321890a901d109ee63f65b76d8d635677425
ysoo14 <ysoo145@gmail.com>
2021-08-23 12:54:15 +0900

    edit train.py

--- train.py
+++ train.py
@@ -7,20 +7,25 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
-from model.model import Model_MNS
+from model.model import Model_MNS, Model_SCP
 from sklearn.metrics import f1_score, confusion_matrix, accuracy_score, classification_report, precision_recall_fscore_support
 from torch.utils.data import DataLoader
 from torch.utils.data import random_split
 from torch.utils.data.distributed import DistributedSampler
 from torch.utils.data.sampler import RandomSampler
 from tqdm import tqdm
-from utils.dataloader import Dataset
+from utils.dataloader import Dataset_SCP, Dataset_MNS
 
 np.random.seed(1234)
 
-def get_data_loader(path, multigpu, batch_size=16, num_workers=0, pin_memory=False):
-    dataset = Dataset(path)
+def get_data_loader(path, task, multigpu, batch_size=16, num_workers=0, pin_memory=False):
+    if task == 'MNS':
+        dataset = Dataset_MNS(path)
+    else:
+        dataset = Dataset_SCP(path)
+
     n_dataset = dataset.len
+
     train_len = int(n_dataset * 0.8)
     valid_len = int(n_dataset * 0.1)
     test_len = n_dataset - train_len - valid_len
@@ -82,9 +87,6 @@ def train_or_eval_model(model, loss_function, dataloader, epoch, device, optimiz
 
         log_prob = model(u, umask) # seq_len, batch, n_classes
 
-        # lp_ = log_prob.transpose(0,1).contiguous().view(-1,log_prob.size()[2]) # batch*seq_len, n_classes
-        # labels_ = label.view(-1) # batch*seq_len
-
         label = label.view(-1)
         loss = loss_function(log_prob, label)
 
@@ -108,11 +110,54 @@ def train_or_eval_model(model, loss_function, dataloader, epoch, device, optimiz
     avg_fscore = round(f1_score(labels,preds,average='weighted')*100,2)
     return avg_loss, avg_accuracy, labels, preds, avg_fscore, epoch 
 
+def train_or_eval_model2(model, loss_function, dataloader, epoch, device, optimizer=None, train=False):
+    losses = []
+    preds = []
+    labels = []
+    assert not train or optimizer!=None
+    if train:
+        model.train()
+    else:
+        model.eval()
+    for data in tqdm(dataloader):
+        if train:
+            optimizer.zero_grad()
+        contexts, utt1s, utt2s, umasks, label, _ = data
+        contexts = contexts.to(device)
+        utt1s = utt1s.to(device)
+        utt2s = utt2s.to(device)
+        label = label.to(device)
+        umasks = umasks.to(device)
+        log_prob = model(contexts, utt1s, utt2s, umasks) # seq_len, batch, n_classes
+
+        label = label.view(-1)
+        loss = loss_function(log_prob, label)
+
+        pred_ = torch.round(log_prob) # batch*seq_len
+        preds.append(pred_.data.cpu().numpy())
+        labels.append(label.data.cpu().numpy())
+
+        losses.append(loss.item())
+        if train:
+            loss.backward()
+            optimizer.step()
+
+    if preds!=[]:
+        preds  = np.concatenate(preds)
+        labels = np.concatenate(labels)
+    else:
+        return float('nan'), float('nan'), [], [], [], float('nan'),[]
+
+    avg_loss = round(np.sum(losses),4)
+    avg_accuracy = round(accuracy_score(labels,preds)*100,2)
+    avg_fscore = round(f1_score(labels,preds,average='weighted')*100,2)
+    return avg_loss, avg_accuracy, labels, preds, avg_fscore, epoch 
+
 if __name__ == '__main__' :
     
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--task', default='MNS', metavar='TASK',
+    parser.add_argument('--task', default='SCP', metavar='TASK',
                         help='training_object')
     parser.add_argument('--multigpu', default=False, metavar='multi-gpu',
                         help='multi-gpu')
@@ -146,44 +191,55 @@ if __name__ == '__main__' :
     dropout=0.3
     n_heads=8
     intermediate_dim=hidden_dim*4
-    n_class=9
+
 
     if args.task == 'MNS':
+        n_class=9
         model = Model_MNS(hidden_dim=hidden_dim, n_layer=n_layer, 
                           dropout=dropout, n_heads=n_heads, 
                           intermediate_dim=intermediate_dim, 
                           n_class=n_class,
                           device=device) #hidden_dim, n_layer, dropout, n_heads, intermediate_dim, n_class
+
+        path = r'./dataset/data.pkl'
+        train_dataloader, valid_dataloader, test_dataloader = get_data_loader(path, args.task, args.multigpu, batch_size=args.batch_size, num_workers=0, pin_memory=False)
+        loss_function = torch.nn.NLLLoss()
 
     else:
-        model = Model_MNS(hidden_dim=hidden_dim, n_layer=n_layer, 
+        n_class=2
+        model = Model_SCP(hidden_dim=hidden_dim, n_layer=n_layer, 
                           dropout=dropout, n_heads=n_heads, 
                           intermediate_dim=intermediate_dim, 
                           n_class=n_class,
                           device=device) #hidden_dim, n_layer, dropout, n_heads, intermediate_dim, n_class
 
+        path = r'./dataset/data2.pkl'
+        train_dataloader, valid_dataloader, test_dataloader = get_data_loader(path, args.task, args.multigpu, batch_size=args.batch_size, num_workers=0, pin_memory=False)
+        loss_function = torch.nn.BCELoss()
     model = model.to(device)
+
     if args.multigpu:
         model = nn.parallel.DistributedDataParallel(model)
-
-    path = r'./dataset/data.pkl'
-
-    train_dataloader, valid_dataloader, test_dataloader = get_data_loader(path, args.multigpu, batch_size=args.batch_size, num_workers=0, pin_memory=False)
 
     optimizer = optim.Adam(model.parameters(),
                            lr=args.lr,
                            weight_decay=args.l2)
     
-    loss_function = torch.nn.NLLLoss()
 
     best_loss, best_label, best_pred, best_mask = None, None, None, None
 
     for e in tqdm(range(n_epochs)):
         start_time = time.time()
-        train_loss, train_acc, _,_,train_fscore, _= train_or_eval_model(model, loss_function,
-                                               train_dataloader, e, device, optimizer, True)
-        valid_loss, valid_acc, _,_,val_fscore, _= train_or_eval_model(model, loss_function, valid_dataloader, e, device)
-        test_loss, test_acc, test_label, test_pred, test_fscore, epoch= train_or_eval_model(model, loss_function, test_dataloader, e, device)
+        if args.task =='MNS':
+            train_loss, train_acc, _,_,train_fscore, _= train_or_eval_model(model, loss_function,
+                                                train_dataloader, e, device, optimizer, True)
+            valid_loss, valid_acc, _,_,val_fscore, _= train_or_eval_model(model, loss_function, valid_dataloader, e, device)
+            test_loss, test_acc, test_label, test_pred, test_fscore, epoch= train_or_eval_model(model, loss_function, test_dataloader, e, device)
+        else:
+            train_loss, train_acc, _,_,train_fscore, _= train_or_eval_model2(model, loss_function,
+                                                train_dataloader, e, device, optimizer, True)
+            valid_loss, valid_acc, _,_,val_fscore, _= train_or_eval_model2(model, loss_function, valid_dataloader, e, device)
+            test_loss, test_acc, test_label, test_pred, test_fscore, epoch= train_or_eval_model2(model, loss_function, test_dataloader, e, device)
 
         if best_loss == None or best_loss > test_loss:
             best_loss, best_label, best_pred =\
